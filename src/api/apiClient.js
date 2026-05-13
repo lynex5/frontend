60ecf151ed20c3222e023841a95312302743a792
Rahul <rahul@Rahuls-MacBook-Air.local>
2026-05-13 21:30:05 +0530

    public link added

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,7 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || 
-  (import.meta.env.MODE === 'production' 
-    ? 'https://backend-g3hl.onrender.com/api' 
-    : 'http://localhost:8080/api');
+export const BASE_URL = import.meta.env.VITE_API_URL || 'https://backend-g3hl.onrender.com/api';
 
 const handleResponse = async (res) => {
     if (!res.ok) {
